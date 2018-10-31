94a34dcf3f4cc770a1ee6a292bfa53f79622d18e
EC2 Default User <ec2-user@ip-172-31-46-108.us-east-2.compute.internal>
2018-10-31 08:45:04 +0000

    Add name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  roote 'messages#index'
+  root 'messages#index'
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_070149) do
+ActiveRecord::Schema.define(version: 2018_10_31_084150) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "massage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
