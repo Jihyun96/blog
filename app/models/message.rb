c40c58a444af9b0928ffa4557e42794f01a081c0
EC2 Default User <ec2-user@ip-172-31-46-108.us-east-2.compute.internal>
2018-11-02 05:51:38 +0000

    new-column

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
     validates :title, length: {maximum:32}, presence: true
     validates :massage, length: {maximum:256}, presence: true
     validates :name, length: {maximum:32}, presence: true
+    validates :金額, length: {maximum:7}, presence: true
 end
